afac84eb8c0a23401dd94b3ce9c2fd54528d51c4
cherryblossom2021 <90602826+cherryblossom2021@users.noreply.github.com>
2023-02-26 11:54:02 +0530

    Update Assign1.py

--- Assign1.py
+++ Assign1.py
@@ -52,3 +52,13 @@ def num(a,b):
         print(a,end=" ")
         return(num(a*(3/4),b))
 num(7,189/64)
+#7
+def num(a,b):
+    if a==b:
+        print(a)
+        return a
+    else:
+        print(a,end=" ")
+        return(num(a+2),b)
+num(0,10)
+
